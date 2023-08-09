151bf0f1f419561835a76dadcb2a716bead149bd
Manisha <114571796+mjthakur1925@users.noreply.github.com>
2023-08-09 21:33:23 +0530

    Add files via upload

--- WarmUp/script.js
+++ WarmUp/script.js
@@ -1,52 +1,36 @@
-class Point{
-    
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
+const cart = [
+    {
+        name: 'Cheese Pizza',
+        price: 500
+    },
+    {
+        name: 'Onion Pizza',
+        price: 100
+    },
+    {
+        name: 'Capcicum Pizza',
+        price: 200
+    },
+    {
+        name: 'Corn Pizza',
+        price: 300
     }
+];
 
-    get getX() {
-        return this.x;
-    }
-
-    get getY() {
-        return this.y;
-    }
-
-    set setX(newX) {
-        this.x = newX;
-    }
-
-    set setY(newY) {
-        this.y = newY;
-    }
-
-    static calculateDist(p1, p2) {
-        const dy = p2.y - p1.y;
-        const dx = p2.x - p1.x;
-        return Math.hypot(dy, dx);
-    }
-
-    static slope(p1, p2) {
-        const dy = p2.y - p1.y;
-        const dx = p2.x - p1.x;
-
-        return dy / dx;
-    }
-
-    static centroid(p1, p2, p3) {
-        const x = (p1.x + p2.x + p3.x) / 3;
-        const y = (p1.y + p2.y + p3.y) / 3;
-
-        return new Point(x, y);
-    }
+function applyDiscount(cart, discount) {
+    return cart.map((pizza) => {
+        return { ...pizza, price: pizza.price * discount / 100 };
+    });
 }
 
+console.table(cart);
+console.table(applyDiscount(cart, 50));
+
+const discountedCart = applyDiscount(cart, 50);
 
 
-const p1 = new Point(1, 2);
-const p2 = new Point(3, 4);
-const p3 = new Point(5, 6);
+function sortCartByPrice(cart) {
+    return cart.sort((pizza1, pizza2) => pizza1.price - pizza2.price);
+}
 
-console.log(Point.centroid(p1, p2, p3));
-console.log(Point.slope(p1, p2));
+console.log(sortCartByPrice(discountedCart));
